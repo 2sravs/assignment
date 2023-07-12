8fd2fb1d81863149a9ffe34e0f5d5c0c2c7ed09d
2Sravs <sravani.chekuri1243#gmail.com>
2023-07-12 14:54:47 +0530

    assignment

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string input = "100";
+
+                int ParsedNumber = int.Parse(input);
+
+                Console.WriteLine("parse number is: \t" + ParsedNumber);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
